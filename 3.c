e52b5cef38cd933bcf41044fa8005355539f75e2
narahari-25 <127509509+narahari-25@users.noreply.github.com>
2023-11-17 02:12:31 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -8,6 +8,38 @@
 
 #define NAMING_SERVER_IP "127.0.0.1"
 #define NAMING_SERVER_PORT 8080
+#define STORAGE_SERVER_IP ""
+
+void communicateWithStorageServer(int storage_server_port) {
+    int ss_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (ss_socket == -1) {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    struct sockaddr_in ss_address;
+    ss_address.sin_family = AF_INET;
+    ss_address.sin_port = htons(storage_server_port);
+    ss_address.sin_addr.s_addr = inet_addr(NAMING_SERVER_IP); // Update this with the actual storage server IP
+
+    if (connect(ss_socket, (struct sockaddr *)&ss_address, sizeof(ss_address)) == -1) {
+        perror("Connection to the storage server failed");
+        close(ss_socket);
+        exit(1);
+    }
+
+    // Send a message to the storage server
+    char message[] = "Hi from the client!";
+    if (send(ss_socket, message, sizeof(message), 0) == -1) {
+        perror("Sending message to storage server failed");
+        close(ss_socket);
+        exit(1);
+    }
+
+    // Add any additional communication with the storage server as needed
+
+    close(ss_socket);
+}
 
 int main() {
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -35,8 +67,7 @@ int main() {
         exit(1);
     }
 
-    // Send the path to the naming server
-    char path[] = "./1.c"; // Change this to the desired path
+    char path[] = "./1.c"; 
     if (send(client_socket, path, sizeof(path), 0) == -1) {
         perror("Sending path to naming server failed");
         close(client_socket);
@@ -52,6 +83,9 @@ int main() {
 
     if (storage_server_port > 0) {
         printf("Storage server port for path '%s': %d\n", path, storage_server_port);
+
+        // Communicate with the storage server
+        communicateWithStorageServer(storage_server_port);
     } else {
         printf("Path '%s' not found on any storage server.\n", path);
     }
