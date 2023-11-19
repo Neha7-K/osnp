86ea4aa93e049a2d99c45dbb817cc129f40d1a9b
narahari-25 <127509509+narahari-25@users.noreply.github.com>
2023-11-19 17:11:33 +0530

    Update 3.h

--- 3.h
+++ 3.h
@@ -3,5 +3,16 @@
 #define NAMING_SERVER_IP "127.0.0.1"
 #define NAMING_SERVER_PORT 8080
 #define STORAGE_SERVER_IP ""
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <time.h>
 void communicateWithStorageServer(int storage_server_port);
 #endif
