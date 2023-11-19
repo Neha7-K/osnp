8e60ae11a57d7d49e91b512c554c1b4fa77869eb
narahari-25 <127509509+narahari-25@users.noreply.github.com>
2023-11-19 17:12:06 +0530

    Update defs.h

--- defs.h
+++ defs.h
@@ -1,18 +1,1 @@
-#ifndef defs_H
-#define defs_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <time.h>
 
-#include "1.h"
-#include "2.h"
-#include "3.h"
-#endif
