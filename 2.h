793b7db3b8dab12afd13cc9b879a84b9f8c87710
narahari-25 <127509509+narahari-25@users.noreply.github.com>
2023-11-19 17:10:24 +0530

    Update 2.h

--- 2.h
+++ 2.h
@@ -5,14 +5,24 @@
 #define NAMING_SERVER_PORT 8080
 #define MAX_COMMAND_SIZE 1024
 #define ACCESSIBLE_PATHS_INTERVAL 60
-
-// struct StorageServerInfo
-// {
-//     char ip_address[16];
-//     int nm_port;
-//     int client_port;
-//     char accessible_paths[4096];
-// };
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <time.h>
+struct StorageServerInfo
+{
+    char ip_address[16];
+    int nm_port;
+    int client_port;
+     char accessible_paths[4096];
+ };
 
 void collectAccessiblePaths(const char *dir_path, char *accessible_paths, int *pos, int size);
 void createFile();
